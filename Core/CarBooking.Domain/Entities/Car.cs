c31fd57fe260408505a36b47db1ce7f8c7a434b9
rmertkayaer <rmertkayaer@hotmail.com>
2024-08-16 02:02:13 +0300

    Add project files.

---
+++ Core/CarBooking.Domain/Entities/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Domain.Entities
+{
+	public class Car
+	{
+		public int CarID { get; set; }
+		public int BrandID { get; set; }
+		public Brand Brand { get; set;  }
+		public string Model { get; set; }
+		public string CoverImageUrl { get; set; }
+		public string BigImageUrl { get; set; }
+        public int Kilometer { get; set; }
+        public string Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public byte Fuel { get; set; }
+		 
+		public List<CarFeature> CarFeatures { get; set; }
+		public List<CarDescription> CarDescriptions { get; set; }
+		public List<CarPricing> CarPricings { get; set; }
+	}
+
+}
---
+++ Core/CarBooking.Domain/Entities/CarFeature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Domain.Entities
+{
+	public class CarFeature
+	{
+        public int CarFeatureID { get; set; }
+        public int CarID { get; set; }
+        public int FeatureID { get; set; }
+        public Car Car { get; set; }
+        public Feature Feature { get; set; }
+        public bool Available { get; set; }
+    }
+}
